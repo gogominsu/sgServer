100cef7382007fb104a91e616f4fd19d155591e9
gogominsu <alstn6608@naver.com>
2021-11-02 14:27:09 +0900

    machineList.html 에 0.5초 주기 자동새로고침 기능 추가

--- src/main/java/dmh/sgserver/SgServerApplication.java
+++ src/main/java/dmh/sgserver/SgServerApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SgServerApplication {
 
     public static void main(String[] args) {
+
         SpringApplication.run(SgServerApplication.class, args);
     }
 
